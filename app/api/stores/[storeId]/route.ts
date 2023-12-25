42708adf6eedbd09923610378ced008099affadb
estuhub <107235863+estuhub@users.noreply.github.com>
2023-12-25 19:40:18 +0700

    create form update route

---
+++ app/api/stores/[storeId]/route.ts
@@ -0,0 +1,34 @@
+import { auth } from "@clerk/nextjs"
+import { NextResponse } from "next/server"
+
+import prismadb from "@/lib/prismadb"
+
+export async function PATCH(
+    req: Request,
+    { params }: { params: { storeId: string }}
+) {
+    try {
+        const { userId } = auth()
+        const { name } = await req.json()
+
+        // Avoid possible errors
+        if (!userId) return new NextResponse("Unauthorized", { status: 401 })
+        if (!name) return new NextResponse("Name is required", { status: 400 })
+        if (!params.storeId) return new NextResponse("Store id is required", { status: 400 })
+
+        // Update Store in DB
+        const store = await prismadb.store.updateMany({
+            where: {
+                id: params.storeId,
+                userId
+            },
+            data: { name }
+        })
+
+        return NextResponse.json(store)
+    } catch (error) {
+        console.log('[STORE_PATCH]', error)
+        return new NextResponse("Internal error", { status: 500 })
+    }
+}
+
