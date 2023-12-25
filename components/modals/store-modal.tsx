e3568b4e520eae0c198d227b05d730c4e6a1022c
estuhub <107235863+estuhub@users.noreply.github.com>
2023-12-25 15:04:40 +0700

    disable input and buttons when loading

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as z from "zod"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import axios from "axios"
@@ -18,6 +19,8 @@ const formSchema = z.object({
 export const StoreModal = () => {
     const { isOpen, onClose } = useStoreModal()
 
+    const [loading, setLoading] = useState(false)
+
     const form = useForm<z.infer<typeof formSchema>>({
         resolver: zodResolver(formSchema),
         defaultValues: {
@@ -27,9 +30,11 @@ export const StoreModal = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try {
+            setLoading(true)
             const response = await axios.post("/api/stores", values)
         } catch (error) {
         } finally {
+            setLoading(false)
         }
     }
 
@@ -50,15 +55,15 @@ export const StoreModal = () => {
                                 <FormItem>
                                     <FormLabel>Name</FormLabel>
                                     <FormControl>
-                                        <Input placeholder="E-commerce" {...field} />
+                                        <Input disabled={loading} placeholder="E-commerce" {...field} />
                                     </FormControl>
                                     <FormMessage />
                                 </FormItem>
                             )}
                         />
                         <div className="pt-6 space-x-2 flex - items-center justify-end w-full">
-                            <Button variant="outline" onClick={onClose}>Cancel</Button>
-                            <Button type="submit">Continue</Button>
+                            <Button disabled={loading} variant="outline" onClick={onClose}>Cancel</Button>
+                            <Button disabled={loading} type="submit">Continue</Button>
                         </div>
                     </form>
                 </Form>
