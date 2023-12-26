1acbc7a9a0e8191dbe811fcc45dad8d80bf5be15
estuhub <107235863+estuhub@users.noreply.github.com>
2023-12-26 18:52:14 +0700

    create basic Table structure using shadCN

--- app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -1,6 +1,8 @@
 import prismadb from "@/lib/prismadb"
+import { format } from "date-fns"
 
-import { BillboardClient } from "@/components/client"
+import { BillboardClient } from "../../../../../components/client"
+import { BillboardColumn } from "./components/columns"
 
 const Billboards = async ({
   params
@@ -16,10 +18,16 @@ const Billboards = async ({
     }
   })
 
+  const formattedBillboards: BillboardColumn[] = billboards.map(item => ({
+    id: item.id,
+    label: item.label,
+    createdAt: format(item.createdAt, "MMMM do, yyyy")
+  }))
+  
   return (
     <div className="flex-col">
         <div className="flex-1 space-y-4 p-8 pt-6">
-            <BillboardClient data={billboards} />
+            <BillboardClient data={formattedBillboards} />
         </div>
     </div>
   )
