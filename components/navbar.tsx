f58bb67be69b1736d5e39fb9342ae66632829791
estuhub <107235863+estuhub@users.noreply.github.com>
2023-12-29 15:11:11 +0700

    add Dark Theme mode

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { redirect } from "next/navigation"
 import { MainNav } from "@/components/main-nav"
 import StoreSwitcher from "./store-switcher"
 import prismadb from "@/lib/prismadb"
+import { ModeToggle } from "./theme-toggle"
 
 const Navbar = async () => {
     const { userId } = auth()
@@ -19,6 +20,7 @@ const Navbar = async () => {
             <StoreSwitcher items={stores} /> {/* Store Switcher using Combobox */}
             <MainNav className="mx-6"/> {/* Routes */}
             <div className="ml-auto flex items-center space-x-4">
+                <ModeToggle />
                 <UserButton afterSignOutUrl="/" /> {/* User Button using Clerk */}
             </div>
         </div>
